59ddeb8d012475c758639f66eb91205cf3ae4eb2
PeterMaffay <johannes@boehlers.de>
2016-10-12 22:32:11 +0200

    Const env added

---
+++ EnvDetector.m
@@ -0,0 +1,56 @@
+classdef EnvDetector < audioPlugin & matlab.System
+    %ENVDETECTOR Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    y = 0;    
+    state = 0;  
+    state2 = 0;
+    end
+    
+    methods  (Access = protected)
+        function out = stepImpl(plugin, in)
+           
+            [samples, channels] = size(in);
+            out = zeros(samples,1);
+            persistent fs tauAttack tauRelease
+            fs = 44100;
+            tauAttack = 0.05;
+            tauRelease = 0.1;
+            
+            taufs = tauRelease * fs;
+            if tauRelease > 0
+                alphaRel = exp(-1 / taufs);
+            else
+                alphaRel = 0;
+            end
+            
+            
+            for i=1:samples
+                if in(i) > plugin.state
+                    plugin.state = in(i);
+                else
+                    plugin.state =alphaRel * plugin.state + (1-alphaRel) *in(i);
+                end
+                in(i) = plugin.state;
+            end
+            
+
+            taufs = tauAttack * fs;
+            if tauAttack > 0
+                alphaAtt = exp(-1 / taufs);
+            else
+                alphaAtt = 0;
+            end
+           
+            
+            for i=1:samples
+                plugin.state2 = alphaAtt * plugin.state2 + (1-alphaAtt) * in(i);
+                out(i) = plugin.state2;
+            end
+            
+        end
+    end
+    
+end
+
